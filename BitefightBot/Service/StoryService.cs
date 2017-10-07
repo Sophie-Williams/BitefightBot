469b8352c3952d4841507465abf559e40e92be5f
Dejan <de.nadlacki@gmail.com>
2017-10-07 18:17:47 +0200

    Game Services

---
+++ BitefightBot/Service/StoryService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BitefightBot.Service
+{
+    public static class StoryService
+    {
+    }
+}
